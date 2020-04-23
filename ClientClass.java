03a97f3fce09e6b03862facf1a1a3c392b3eede7
Baby-girls <54897128+Baby-girls@users.noreply.github.com>
2020-04-23 10:30:53 +0800

    6 原型模式管理器

--- ClientClass.java
+++ ClientClass.java
@@ -3,15 +3,15 @@ package pro;
 public class ClientClass {
 	public static void main(String[] args) {
 		MyFruit fru1 = new Apple();
-		MyFruit fru2 = fru1;
-		// MyFruit fru2=(Apple)fru1.clone();
-		fru1.Display();
-		fru2.Display();
-		System.out.println("fru1:" + fru1.hashCode());
-		System.out.println("fru2:" + fru1.hashCode());
-		// System.out.println("fru1:" + fru1.toString());
-		// System.out.println("fru2:" + fru1.toString());
-		// System.out.println(fru1==fru2);
+		MyFruit fru2 = new Banana();
+		MyFruitStore.Add(1, fru1);
+		MyFruitStore.Add(2, fru2);
+		MyFruitStore.Add(3, new Apple());
+		MyFruitStore.Add(4, new Banana());
+
+		MyFruit fru = (MyFruit) MyFruitStore.Get(3);
+		fru.Display();
+
 	}
 
 }
