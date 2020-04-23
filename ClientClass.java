194dd47158b4634c57740f2d63d82091b3803964
Baby-girls <54897128+Baby-girls@users.noreply.github.com>
2020-04-23 10:47:33 +0800

    7 单例模式

--- ClientClass.java
+++ ClientClass.java
@@ -4,13 +4,14 @@ public class ClientClass {
 	public static void main(String[] args) {
 		MyFruit fru1 = new Apple();
 		MyFruit fru2 = new Banana();
-		MyFruitStore.Add(1, fru1);
-		MyFruitStore.Add(2, fru2);
-		MyFruitStore.Add(3, new Apple());
-		MyFruitStore.Add(4, new Banana());
-
-		MyFruit fru = (MyFruit) MyFruitStore.Get(3);
+		MyFruitStore mfs = MyFruitStore.GetfruitStore();
+		mfs.Add(1, fru1);
+		mfs.Add(2, fru1);
+		mfs.Add(3, new Apple());
+		mfs.Add(4, new Banana());
+		MyFruit fru = (MyFruit) mfs.Get(3);
 		fru.Display();
+		System.out.println("mfs1:" + mfs.toString());
 
 	}
 
--- MyFruitStore.java
+++ MyFruitStore.java
@@ -3,13 +3,25 @@ package pro;
 import java.util.Hashtable;
 
 public class MyFruitStore {
-	private static Hashtable fruittable = new Hashtable<Integer, MyFruit>();
+	private static Hashtable fruittable = null;
+	private static MyFruitStore fruitstore = null;
 
-	public static void Add(Integer key, MyFruit fruit) {
+	private MyFruitStore() {
+		fruittable = new Hashtable<Integer, MyFruit>();
+	}
+
+	public static MyFruitStore GetfruitStore() {
+		if (fruitstore == null) {
+			fruitstore = new MyFruitStore();
+		}
+		return fruitstore;
+	}
+
+	public void Add(Integer key, MyFruit fruit) {
 		fruittable.put(key, fruit);
 	}
 
-	public static MyFruit Get(Integer key) {
+	public MyFruit Get(Integer key) {
 		MyFruit fruit = (MyFruit) fruittable.get(key);
 		return (MyFruit) fruit.clone();
 	}
