1e441acfed69d036da612b0a7e2dc2efaf40f2ae
Baby-girls <54897128+Baby-girls@users.noreply.github.com>
2020-04-28 15:34:34 +0800

    24 解释器模式

---
+++ Expression1.java
@@ -0,0 +1,8 @@
+package Int;
+
+public class Expression1 implements AbstractExpression {
+	public void explain(Context context) {
+		System.out.println("print()"); // 转化java输出语句
+	}
+
+}
---
+++ Expression2.java
@@ -0,0 +1,11 @@
+package Int;
+
+public class Expression2 implements AbstractExpression {
+
+	@Override
+	public void explain(Context context) {
+		// TODO Auto-generated method stub
+		System.out.println("def ");
+	}
+
+}
