94fcd8c20b664ec1d7d2a1bc604597bdf36fe6a9
Baby-girls <54897128+Baby-girls@users.noreply.github.com>
2020-04-28 10:11:34 +0800

    18 迭代器模式

--- Television.java
+++ Television.java
@@ -1,15 +1,6 @@
-package Fac;
+package Tterator;
 
-public class Television {
-
-	public void on() {
-		// TODO Auto-generated method stub
-		System.out.println("电视打开");
-	}
-
-	public void off() {
-		// TODO Auto-generated method stub
-		System.out.println("电视关闭");
-	}
+public interface Television {
+	TVIterator createIterator();
 
 }
